59f655d0c57413d5b00c550d9dbccf53cbadaab5
Joao Breno Gomes <138957799+Jhonnee101@users.noreply.github.com>
2025-07-08 20:11:55 -0300

    add

--- dp_ses/dp_ses/urls.py
+++ dp_ses/dp_ses/urls.py
@@ -1,30 +1,16 @@
-"""
-URL configuration for dp_ses project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# dp_ses/urls.py
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from colaboradores_management import views as app_views
 
-
+app_name = 'dp_ses'  # ← ESSA LINHA REGISTRA O NAMESPACE!
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('admin/', admin.site.urls),
+    path('login/',  auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('dashboard/', app_views.dashboard_view, name='dashboard'),
 
+    path('colaboradores/', include(('colaboradores_management.urls', 'colaboradores_management'), namespace='colaboradores')),
 ]
-
