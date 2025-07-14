bec48022e970ea2412d864500ac2f2bdc5f4fa37
Joao Breno Gomes <138957799+Jhonnee101@users.noreply.github.com>
2025-07-14 13:21:53 -0300

    att

--- dp_ses/settings.py
+++ dp_ses/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-53@wq@km4ph*8&u0qdg0z(^0qx%wvks4%vg#_%9whbrp38%c-x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
