6a930a48d578d558a52cfb27c274a9556119b846
Joao Breno Gomes <138957799+Jhonnee101@users.noreply.github.com>
2025-07-20 21:59:53 -0300

    add

--- dp_ses/settings.py
+++ dp_ses/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'dp_ses.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'dba.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dp_ses',
+        'USER': 'joaob',
+        'PASSWORD': '36457454',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
